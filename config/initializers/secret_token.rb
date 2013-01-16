56adbf010a3b4cde484ad586b9230516429f20a4
Brooks Swinnerton <bswinnerton@gmail.com>
2013-01-16 15:56:18 +0000

    initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ezproxy::Application.config.secret_token = 'b361bb3932404d08a14d81682e13a05104e6cc04be293140fec2f27b1f8c340d163a52477d75325ef81f86beb3559a5e77e07460ed2d613fc3c79e89e39a1856'
