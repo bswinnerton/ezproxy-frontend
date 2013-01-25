47e61e3aee77b27ebd92bc7f06e91675422aafc0
Brooks Swinnerton <bswinnerton@gmail.com>
2013-01-25 16:46:31 +0000

    removing old relations from model

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,4 @@
 class Resource < ActiveRecord::Base
   attr_accessible :domain, :host, :title, :url, :is_deleted
   default_scope where(:is_deleted => false)
-  has_many :djs
-  has_many :hjs
-  has_many :options
 end
