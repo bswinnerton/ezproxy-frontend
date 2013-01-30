7ceb7e01553022663647af5ecb8a73fe0a1b7c04
Brooks Swinnerton <bswinnerton@gmail.com>
2013-01-30 20:32:04 +0000

    adding schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130123155045) do
+ActiveRecord::Schema.define(:version => 20130130192127) do
+
+  create_table "options", :force => true do |t|
+    t.integer  "resource_id"
+    t.string   "option"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "resources", :force => true do |t|
     t.string   "title"
