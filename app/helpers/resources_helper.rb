0794b53e45047bf9ef2608d7e0ddf732204bf8bb
Brooks Swinnerton <bswinnerton@gmail.com>
2013-02-21 17:41:23 +0000

    adding dynamic space generator to #show

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,2 +1,10 @@
 module ResourcesHelper
+
+  def determine_num_of_spaces(word)
+    column_length = 8
+    word_length = word.length
+    num_of_spaces = column_length - word_length
+    raw "&nbsp;" * num_of_spaces
+  end
+
 end
