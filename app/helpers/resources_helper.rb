6e659069e4706bba0c83ee5044e3cba93f18a300
Brooks Swinnerton <bswinnerton@gmail.com>
2013-02-28 16:35:53 +0000

    fixing spacing issue

--- app/helpers/resources_helper.rb
+++ app/helpers/resources_helper.rb
@@ -1,11 +1,11 @@
 module ResourcesHelper
 
-  def determine_num_of_spaces(word, type="html")
+  def determine_num_of_spaces(word, type)
     column_length = 8
     word_length = word.length
     num_of_spaces = column_length - word_length
-    raw "&nbsp;" * num_of_spaces if type == "html"
-    raw " " * num_of_spaces if type == "text"
+    return raw "&nbsp;" * num_of_spaces if type == :html
+    return raw " " * num_of_spaces if type == :text
   end
 
 end
